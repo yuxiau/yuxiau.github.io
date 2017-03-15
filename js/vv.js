2d28daa3a59d56f9eb45c4f9aee2c14cd5ccc451
yuxiau <yuxibk@gmail.com>
2017-03-15 15:00:22 +0000

    update

--- js/vv.js
+++ js/vv.js
@@ -1,1 +1,7 @@
-var vglnk={key:"04e318f5f77f6507550f2b2b7d6d119b"};!function(n,t){var e=n.createElement(t);e.type="text/javascript",e.async=!0,e.src="//cdn.viglink.com/api/vglnk.js";var i=n.getElementsByTagName(t)[0];i.parentNode.insertBefore(e,i)}(document,"script");
+  var vglnk = { key: '04e318f5f77f6507550f2b2b7d6d119b' };
+
+  (function(d, t) {
+    var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
+    s.src = '//cdn.viglink.com/api/vglnk.js';
+    var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
+  }(document, 'script'));
